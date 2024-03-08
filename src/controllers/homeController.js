0c7930c5dcd431e0d7eb3f7e1f1467adde57df55
Thanh <a01204496068@gmail.com>
2024-03-08 19:06:03 +0700

    show-user

--- src/controllers/homeController.js
+++ src/controllers/homeController.js
@@ -1,33 +1,45 @@
 const connection = require("../config/database");
+const { getAllUsers } = require("../services/CRUDService");
 
-const getHomepage = (req, res) => {
-  res.render("sample");
+const getHomepage = async (req, res) => {
+  let results = await getAllUsers();
+  console.log(results);
+  return res.render("home.ejs", { listUsers: results });
 };
 
 const abcPage = (req, res) => {
-  return res.render("home.ejs");
+  return res.render("sample");
 };
 
-const postCreateUser = (req, res) => {
-  let email = req.body.email;
-  let name = req.body.name;
-  let city = req.body.city;
+const Create = (req, res) => {
+  return res.render("create.ejs");
+};
+const postCreateUser = async (req, res) => {
+  let { email, name, city } = req.body;
 
-  console.log(email, name, city);
+  // console.log(email, name, city);
+  // connection.query(` INSERT INTO Users (email, name, city) VALUES (?,?,?)`, [
+  //   email,
+  //   name,
+  //   city,
+  // ]);
 
-  connection.query(
+  // connection.query("SELECT * FROM Users", function (err, results, fields) {
+  //   console.log("Query results:", results);
+  //   }
+
+  let [results, fields] = await connection.query(
     ` INSERT INTO Users (email, name, city) VALUES (?,?,?)`,
-    [email, name, city],
-    function (err, results) {
-      console.log(results);
-
-      res.send(`create user successfully`);
-    }
+    [email, name, city]
   );
+  console.log("check result", results);
+
+  res.send("Create User succesed");
 };
 
 module.exports = {
   getHomepage,
   abcPage,
   postCreateUser,
+  Create,
 };
--- src/routes/web.js
+++ src/routes/web.js
@@ -5,12 +5,15 @@ const {
   getHomepage,
   abcPage,
   postCreateUser,
+  Create,
 } = require("../controllers/homeController");
 
 router.get("/", getHomepage);
 // router.get("/", getHomepage);
 
 router.get("/abc", abcPage);
+
+router.get("/create", Create);
 
 router.post("/create-user", postCreateUser);
 
